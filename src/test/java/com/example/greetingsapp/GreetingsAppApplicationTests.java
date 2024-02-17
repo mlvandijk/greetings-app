145e20a513a2e254985c53a91176eb85c439adab
marit.van.dijk <mlvandijk@gmail.com>
2024-02-17 06:19:17 +0100

    Initial commit

---
+++ src/test/java/com/example/greetingsapp/GreetingsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greetingsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
